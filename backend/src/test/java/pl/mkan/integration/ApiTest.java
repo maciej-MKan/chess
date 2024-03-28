86834526f1a06d52496508495361d77cf06280b0
maciej-MKan <kanabaj.maciej@gmail.com>
2024-03-29 00:02:53 +0100

    backend-add_init_game_controller_test

--- backend/src/test/java/pl/mkan/integration/ApiTest.java
+++ backend/src/test/java/pl/mkan/integration/ApiTest.java
@@ -1,5 +1,6 @@
 package pl.mkan.integration;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static pl.mkan.controller.rest.GameController.API_PATH;
 
 @ActiveProfiles("test")
@@ -22,7 +24,13 @@ public class ApiTest {
                 .when()
                 .get("http://localhost:" + port + API_PATH + "/game")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("pieces.size()", equalTo(32))
+                .body("pieces.find { it.id == 11 }.type", equalTo("ROOK"))
+                .body("pieces.find { it.id == 12 }.color", equalTo("WHITE"))
+                .body("pieces.find { it.id == 13 }.position.row", equalTo(0))
+                .body("pieces.find { it.id == 13 }.position.column", equalTo(2));
     }
 
 }
