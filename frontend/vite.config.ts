cf52282615e046bf487755b039b94551008afb24
maciej-MKan <kanabaj.maciej@gmail.com>
2024-04-08 00:15:02 +0200

    frontend-add_docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
   // test: {
   //   globals: true,
   //   environment: 'jsdom',
