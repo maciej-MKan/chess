5de67a074dd80a186219d281f51f22920d4261bd
maciej-MKan <kanabaj.maciej@gmail.com>
2024-04-12 00:01:09 +0200

    frontend-add_chessboard_test

---
+++ frontend/__test__/integration/Chesseboard.test.jsx
@@ -0,0 +1,18 @@
+import {describe, test, expect, vi} from "vitest";
+import {render, screen} from "@testing-library/react";
+import Chessboard from "../../src/views/board/bord";
+
+describe('Chess board test', () => {
+    test('renders wait message before api response', () => {
+        render(<Chessboard/>);
+
+        expect(screen.getByText(/Wait for API response/i)).toBeDefined();
+
+    })
+    test('renders chessboard after api response', () => {
+        global.fetch = vi.fn().mockRejectedValueOnce(new Error('Backend unavailable'));
+
+        expect(screen.getByText(/Backend unavailable/i)).toBeDefined();
+    })
+
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Chessboard from "./views/board/bord";
-import { jsonData } from "./api/example";
 
 
 const App = () => {
   return (
       <div>
         <h1>Chessboard</h1>
-        <Chessboard pieces={jsonData.pieces} />
+        <Chessboard/>
       </div>
   );
 }
