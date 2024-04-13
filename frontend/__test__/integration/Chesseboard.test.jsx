4bd4b7776d2e1c87deba460b935d535a37454fa7
maciej-MKan <kanabaj.maciej@gmail.com>
2024-04-13 21:33:45 +0200

    frontend-add_chessboard_async_test

--- frontend/__test__/integration/Chesseboard.test.jsx
+++ frontend/__test__/integration/Chesseboard.test.jsx
@@ -1,7 +1,8 @@
-import {describe, test, expect, vi} from "vitest";
+import {describe, test, expect, vi, it} from "vitest";
 import {render, screen} from "@testing-library/react";
 import Chessboard from "../../src/views/board/bord";
 
+vi.stubGlobal('fetch', vi.fn());
 describe('Chess board test', () => {
     test('renders wait message before api response', () => {
         render(<Chessboard/>);
@@ -13,6 +14,15 @@ describe('Chess board test', () => {
         global.fetch = vi.fn().mockRejectedValueOnce(new Error('Backend unavailable'));
 
         expect(screen.getByText(/Backend unavailable/i)).toBeDefined();
+    })
+
+    it('throws an error when fetch fails', async () =>{
+        fetch.mockResolvedValueOnce({
+            ok: false
+        });
+
+        render(<Chessboard/>);
+        await expect(screen.getByText(/none/i)).toBeDefined();
     })
 
 });
