d4c82d5242d350395a452a36fbbcc68ad93ebd45
maciej-MKan <kanabaj.maciej@gmail.com>
2024-04-28 00:12:00 +0200

    backend-fix_available_moves_request

--- backend/src/main/java/pl/mkan/controller/dto/AvailableMovesRequestDTO.java
+++ backend/src/main/java/pl/mkan/controller/dto/AvailableMovesRequestDTO.java
@@ -1,8 +1,9 @@
 package pl.mkan.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
-public record AvailableMovesRequestDTO(@NotEmpty List<PieceDTO> pieces, MoveDTO prevMove) {
+public record AvailableMovesRequestDTO(@NotEmpty List<PieceDTO> pieces, @JsonProperty("move") MoveDTO prevMove) {
 }
