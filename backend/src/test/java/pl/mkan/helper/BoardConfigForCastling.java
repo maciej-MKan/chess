4eb8187d752e19af52a7f3a5c2d35e8f56b7fe73
maciej-MKan <kanabaj.maciej@gmail.com>
2024-06-04 00:59:27 +0200

    backend-add_castling_test

---
+++ backend/src/test/java/pl/mkan/helper/BoardConfigForCastling.java
@@ -0,0 +1,63 @@
+package pl.mkan.helper;
+
+import org.junit.jupiter.params.provider.Arguments;
+import pl.mkan.controller.dto.PositionDTO;
+
+import java.util.stream.Stream;
+
+public class BoardConfigForCastling {
+    public static Stream<Arguments> playerCastling() {
+        return Stream.of(
+                Arguments.of("""
+                                {"pieces": [{
+                                                "id": 88,
+                                                "type": "KING",
+                                                "color": "BLACK",
+                                                "position": {
+                                                    "row": 7,
+                                                    "column": 0
+                                                },
+                                                "moved": true
+                                            },
+                                            {
+                                                "id": 99,
+                                                "type": "KING",
+                                                "color": "WHITE",
+                                                "position": {
+                                                    "row": 0,
+                                                    "column": 5
+                                                },
+                                                "moved": false
+                                            },
+                                            {
+                                                "id": 11,
+                                                "type": "PAWN",
+                                                "color": "WHITE",
+                                                "position": {
+                                                    "row": 3,
+                                                    "column": 3
+                                                },
+                                                "moved": true
+                                            },
+                                            {
+                                                "id": 23,
+                                                "type": "BISHOP",
+                                                "color": "BLACK",
+                                                "position": {
+                                                    "row": 0,
+                                                    "column": 2
+                                                },
+                                                "moved": false
+                                            }],
+                                "move": {
+                                            "srcColumn": 4,
+                                            "srcRow": 7,
+                                            "destColumn": 0,
+                                            "destRow": 7
+                                        }
+                                }""",
+                        88,
+                        new PositionDTO(7, 2))
+        );
+    }
+}
---
+++ backend/src/test/java/pl/mkan/integration/CastlingTest.java
@@ -0,0 +1,40 @@
+package pl.mkan.integration;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import pl.mkan.controller.dto.PositionDTO;
+
+import static io.restassured.RestAssured.given;
+import static pl.mkan.controller.rest.GameController.API_PATH;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class CastlingTest {
+    @LocalServerPort
+    private int port;
+
+    @ParameterizedTest
+    @MethodSource({"pl.mkan.helper.BoardConfigForCastling#playerCastling"})
+    public void shouldDetectEnPassant(String requestBody, int castlingKingId, PositionDTO expectedPosition) {
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("http://localhost:" + port + API_PATH + "/game")
+                .then()
+                .statusCode(200)
+                .body("pieces.find { it.id ==" + castlingKingId + " }.position.row",
+                        Matchers.equalTo(expectedPosition.row()))
+                .body("pieces.find { it.id ==" + castlingKingId + " }.position.column",
+                        Matchers.equalTo(expectedPosition.column()))
+                .body("pieces.find { it.type == 'ROOK' }.position.row",
+                        Matchers.equalTo(expectedPosition.row()));
+    }
+}
