44e44005cedb725d868d962d5fc86bf1ac1c3136
maciej-MKan <kanabaj.maciej@gmail.com>
2024-06-16 00:36:10 +0200

    backend-fix_validator

--- backend/src/main/java/pl/mkan/controller/dto/MoveRequestDTO.java
+++ backend/src/main/java/pl/mkan/controller/dto/MoveRequestDTO.java
@@ -4,9 +4,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import pl.mkan.controller.dto.enums.PieceColor;
+import pl.mkan.controller.dto.validators.UniquePiecesPosition;
 
 import java.util.List;
 
-//@UniquePiecesPosition
+@UniquePiecesPosition
 public record MoveRequestDTO(@Valid @NotEmpty List<PieceDTO> pieces, MoveDTO move, @NotNull PieceColor playerColor) {
 }
--- backend/src/main/java/pl/mkan/controller/dto/validators/UniquePiecesPositionValidator.java
+++ backend/src/main/java/pl/mkan/controller/dto/validators/UniquePiecesPositionValidator.java
@@ -3,6 +3,7 @@ package pl.mkan.controller.dto.validators;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import pl.mkan.controller.dto.BoardDTO;
+import pl.mkan.controller.dto.MoveRequestDTO;
 import pl.mkan.controller.dto.PieceDTO;
 import pl.mkan.controller.dto.PositionDTO;
 
@@ -10,15 +11,37 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class UniquePiecesPositionValidator implements
-        ConstraintValidator<UniquePiecesPosition, BoardDTO> {
+        ConstraintValidator<UniquePiecesPosition, Object> {
 
     @Override
-    public boolean isValid(BoardDTO boardDTO, ConstraintValidatorContext constraintValidatorContext) {
-        if (boardDTO == null || boardDTO.pieces() == null) {
+    public boolean isValid(Object board, ConstraintValidatorContext constraintValidatorContext) {
+        if (board == null) {
             return true;
         }
 
         Set<PositionDTO> positions = new HashSet<>();
+
+        try {
+            return validateBoard((BoardDTO) board, positions);
+        } catch (ClassCastException e) {
+            try {
+                return validateBoard((MoveRequestDTO) board, positions);
+            } catch (ClassCastException e2) {
+                return false;
+            }
+        }
+    }
+
+    private boolean validateBoard(BoardDTO boardDTO, Set<PositionDTO> positions) {
+        for (PieceDTO piece : boardDTO.pieces()) {
+            if (!positions.add(piece.position())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateBoard(MoveRequestDTO boardDTO, Set<PositionDTO> positions) {
         for (PieceDTO piece : boardDTO.pieces()) {
             if (!positions.add(piece.position())) {
                 return false;
