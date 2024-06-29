725661c1a5b98037efc2abd95bd47128e19c620b
maciej-MKan <kanabaj.maciej@gmail.com>
2024-06-29 21:50:02 +0200

    backend_add_logging_user_info

--- backend/src/main/java/pl/mkan/controller/rest/SecurityController.java
+++ backend/src/main/java/pl/mkan/controller/rest/SecurityController.java
@@ -6,7 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +22,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping(path = "/login")
-    public ResponseEntity<String> login() {
-        return ResponseEntity.ok("Welcome");
+    public ResponseEntity<String> login(@AuthenticationPrincipal OAuth2User principal) {
+        log.info("User [{}] login", principal.getName());
+        OAuth2AuthenticationToken authentication =
+                (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            log.info(authentication.getAuthorizedClientRegistrationId());
+        }
+        return ResponseEntity.ok("Welcome " + principal.getAttribute("name"));
     }
   
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public String logout(
+            @AuthenticationPrincipal OAuth2User user,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws Exception {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return "redirect:/";
+        log.info("User [{}] log out", user.getName());
+        return "Bye " + user.getAttribute("name");
     }
 }
