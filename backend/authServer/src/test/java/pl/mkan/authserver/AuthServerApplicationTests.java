be85a3bec6c6c015b597ca9845c3828c35f9235e
maciej-MKan <kanabaj.maciej@gmail.com>
2024-07-10 22:59:37 +0200

    backend-add_auth_server

---
+++ backend/authServer/src/test/java/pl/mkan/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkan.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
