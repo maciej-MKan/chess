28c1d2de212d0b17c3a75d64bc6c8f69cd154ef3
maciej-MKan <kanabaj.maciej@gmail.com>
2024-07-25 23:28:14 +0200

    backend-add_user_preferences

---
+++ backend/src/main/java/pl/mkan/persistence/model/UserPreferences.java
@@ -0,0 +1,25 @@
+package pl.mkan.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class UserPreferences {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String defaultColor;
+}
