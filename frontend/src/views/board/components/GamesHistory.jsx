b8c106a05c1f6846bfde7239afa5260190ebbd38
maciej-MKan <kanabaj.maciej@gmail.com>
2024-08-31 00:31:42 +0200

    frontend-fix_games_history_modal

--- frontend/src/views/board/components/GamesHistory.jsx
+++ frontend/src/views/board/components/GamesHistory.jsx
@@ -1,30 +1,39 @@
 import React from "react";
-import './GamesHistory.css'
+import './GamesHistory.css';
 import Modal from "react-modal";
+import {useSelector} from "react-redux";
 
-const GamesHistory = (games, isOpen, onClose) => {
-    if (games) {
-        return (
-            <Modal
-                isOpen={isOpen}
-                onRequestClose={onClose}
-                contentLabel='Games History Modal'
-            >
-                <div className="games-history">
-                    <h2>Games History</h2>
+const GamesHistory = ({isOpen, onClose}) => {
+    const games = useSelector((state) => state.game.historicalGames);
+
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onClose}
+            contentLabel='Games History Modal'
+        >
+            <div className="games-history">
+                <h2>Games History</h2>
+                {games.length > 0 ? (
                     <ul>
-                        {games?.map((game, index) => (
+                        {games.map((game, index) => (
                             <li key={index} onClick={() => console.log(game)} className="historical-game">
                                 <div>
-                                    {game.gameId}
+                                    {game.actualBoardState.gameId ? game.actualBoardState.gameId : "No gameId available"}
                                 </div>
                             </li>
                         ))}
                     </ul>
-                </div>
-            </Modal>
-        );
-    }
-}
+                ) : (
+                    <p>No games history available.</p>
+                )}
+            </div>
+        </Modal>
+    );
+};
 
-export default GamesHistory
+export default GamesHistory;
