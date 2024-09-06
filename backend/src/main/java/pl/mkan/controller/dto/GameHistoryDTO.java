36f4fc7b328f252ff2102cab221c444a2a95d2f7
maciej-MKan <kanabaj.maciej@gmail.com>
2024-09-06 17:47:04 +0200

    backend-add_game-timestamp

--- backend/src/main/java/pl/mkan/controller/dto/GameHistoryDTO.java
+++ backend/src/main/java/pl/mkan/controller/dto/GameHistoryDTO.java
@@ -2,8 +2,9 @@ package pl.mkan.controller.dto;
 
 import pl.mkan.controller.dto.enums.PieceColor;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public record GameHistoryDTO(BoardDTO actualBoardState, List<MoveHistoryDTO> movesHistory,
-                             PieceColor playerColor) {
+                             PieceColor playerColor, ZonedDateTime gameStartDate) {
 }
