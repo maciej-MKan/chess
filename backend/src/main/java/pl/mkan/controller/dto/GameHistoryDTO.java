2b976e4953a00bb49a621a539f35c435a1e001be
maciej-MKan <kanabaj.maciej@gmail.com>
2024-09-17 22:22:28 +0200

    backend-add_game_over_field

--- backend/src/main/java/pl/mkan/controller/dto/GameHistoryDTO.java
+++ backend/src/main/java/pl/mkan/controller/dto/GameHistoryDTO.java
@@ -1,10 +1,11 @@
 package pl.mkan.controller.dto;
 
+import jakarta.annotation.Nullable;
 import pl.mkan.controller.dto.enums.PieceColor;
 
 import java.time.ZonedDateTime;
 import java.util.List;
 
 public record GameHistoryDTO(BoardDTO actualBoardState, List<MoveHistoryDTO> movesHistory,
-                             PieceColor playerColor, ZonedDateTime gameStartDate) {
+                             PieceColor playerColor, ZonedDateTime gameStartDate, @Nullable GameOverDTO gameState) {
 }
