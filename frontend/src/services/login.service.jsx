eb6c7802256138a7c53f1b686bfabd8c92d340d2
maciej-MKan <kanabaj.maciej@gmail.com>
2024-09-24 23:29:16 +0200

    frontend-add_fetch_game_history_to_login_service

--- frontend/src/services/login.service.jsx
+++ frontend/src/services/login.service.jsx
@@ -1,13 +1,16 @@
 import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import {fetchUserDetails} from "../api/user";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {setUsername} from "../redux/userSlice";
 import {logIn} from "../redux/authSlice";
+import {getGamesHistory} from "../api/game";
+import {setHistoricalGames} from "../redux/gameSlice";
 
 export const LoginSuccess = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const loginIn = useSelector((state) => state.auth.isLoginIn);
 
     const onLoginCorrect = (user) => {
         dispatch(setUsername(user.name));
@@ -22,4 +25,24 @@ export const LoginSuccess = () => {
                 data.name ? onLoginCorrect(data) : navigate("/login");
             })
     }, []);
+
+    useEffect(() => {
+        getGamesHistory()
+            .then(result => {
+                dispatch(setHistoricalGames(result));
+                if (result) loadGame(result.pop());
+            })
+            .catch(error => console.log(error))
+    }, [loginIn === true])
+
+    const loadGame = (gameData) => {
+        const gameState = {
+            boardState: gameData.actualBoardState,
+            movesHistory: gameData.movesHistory,
+            playerColor: gameData.playerColor
+        };
+        console.log("load prev game");
+        sessionStorage.setItem('chessGameState', JSON.stringify(gameState));
+        navigate("/game");
+    }
 }
